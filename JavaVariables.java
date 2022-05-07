f5b9535b0a1502f481922d4bbf4fc30bae14c3ee
josuecahuana <jcahuanaag@unsa.edu.pe>
2022-05-07 12:19:07 -0500

    Cree la clase JavaVariables

---
+++ JavaVariables.java
@@ -0,0 +1,28 @@
+public class JavaVariables {
+    public static void main(String[] args){
+        //En Java hay varia variables de distintos tipos, cada uno retiene diferentes tipos de datos
+
+        String primeraVariable = "Mi primera variable";
+        System.out.println(primeraVariable);
+
+        int miNumero = 25;
+        System.out.println(miNumero);
+
+        /*Los String almacenan lineas de texto, mientras que los int almacenan enteros
+          mientras que los float y double pueden almacenar numeros con decimales
+        */
+
+        Boolean JavaEsDivertido = true;
+        System.out.println(JavaEsDivertido);
+
+        char LetraA = 'a';
+        System.out.println(LetraA);
+
+        //Los boolean almacenan valores positivos o negativos y los char almacenan un unico caracter
+
+        final double Constante = 3.14;
+
+        //Se pueden declarar variables finales poniendo final comienzo, estas variables no podran ser modificadas
+    }
+    
+}
